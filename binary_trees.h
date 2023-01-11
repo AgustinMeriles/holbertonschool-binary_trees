bf4e8fc7d28d5da44b5c8b69f9aea11995d9866d
agustinmeriles <agustinmeriles@hotmail.com>
2023-01-11 18:01:06 -0300

    binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -22,13 +22,8 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
-//BASIC SEARCH TREE
-typedef struct binary_tree_s bst_t;
-//AVL TREE
-typedef struct binary_tree_s avl_t;
-//MAX BINARY HEAP
-typedef struct binary_tree_s heap_t;
 
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);

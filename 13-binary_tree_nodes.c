1e25a6f37652e87638cbad91781f8e52f754718c
NexoNeko <meresidamente@hotmail.com>
2023-01-12 10:38:55 -0300

    Create 13-binary_tree_nodes.c

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - counts nodes w/at least 1 child @ Btree
+ * @tree: Pointer to the tree
+ *
+ * Return: 0 if tree is null, num of nodes otherwise.
+ */
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes = 0;
+
+	if (!tree)
+		return(0);
+
+	nodes += (tree->left || tree->right) ? 1 : 0;
+	nodes += binary_tree_nodes(tree->left);
+	nodes += binary_tree_nodes(tree->right);
+	return (nodes);
+}

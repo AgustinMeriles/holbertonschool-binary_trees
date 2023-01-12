c7a6e6c611aca0d86440b7f4fadb4b0079ef8857
NexoNeko <meresidamente@hotmail.com>
2023-01-12 10:05:06 -0300

    Update 12-binary_tree_leaves.c

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,8 +1,9 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_leaves - counts the leaves in a binary tree
+ * @tree: root node of tree
  *
- *
- *
+ * Return: 0 if tree is null, 1 if no leaves, otherwise leave num
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
